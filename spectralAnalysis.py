87ce00003a7f413537f346623529cafbef0e7d4f
srsummerson <srsummerson@gmail.com>
2015-07-24 13:41:05 -0700

    fixing bug in LFPSpectrumSingleChannel

--- spectralAnalysis.py
+++ spectralAnalysis.py
@@ -5,6 +5,9 @@ from scipy import fft, arange
 
 
 def LFPSpectrumSingleChannel(tankname,channel):
+	"""
+	Adopted from: http://glowingpython.blogspot.com/2011/08/how-to-plot-frequency-spectrum-with.html
+	"""
 	r = io.TdtIO(dirname=tankname)
 	bl = r.read_block(lazy=False)
 
@@ -17,7 +20,9 @@ def LFPSpectrumSingleChannel(tankname,channel):
 				data = analogsig
 				data_times = analogsig.times
 				n = len(data) # length of the signal
+				k = arange(n)
  				T = n/Fs
+ 				frq = k/T # two sides frequency range
  				frq = frq[range(n/2)] # one side frequency range
 
  				Y = fft(data)/n # fft computing and normalization
