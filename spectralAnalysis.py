e51b7acedc0510b840f136dce1cfd57acd44114d
srsummerson <srsummerson@gmail.com>
2015-07-24 15:32:14 -0700

    fixing bug in LFPSpectrumSingleChannel

--- spectralAnalysis.py
+++ spectralAnalysis.py
@@ -1,17 +1,15 @@
 from neo import io
 from numpy import sin, linspace, pi
-import matplotlib
-matplotlib.use('nbagg')
 from matplotlib import pyplot as plt
 from scipy import fft, arange
-
+from pylab import specgram
 
 def LFPSpectrumSingleChannel(tankname,channel):
 	"""
 	Adopted from: http://glowingpython.blogspot.com/2011/08/how-to-plot-frequency-spectrum-with.html
 	"""
 	r = io.TdtIO(dirname=tankname)
-	bl = r.read_block(lazy=False)
+	bl = r.read_block(lazy=False,cascade=True)
 
 	for block in bl.segments:
 		for analogsig in block.analogsignals:
@@ -36,5 +34,21 @@ def LFPSpectrumSingleChannel(tankname,channel):
  				plt.xlabel('Freq (Hz)')
  				plt.ylabel('|Y(freq)|')
  				#title('Channel %f' %channel)
- 				plt.show()
+ 				plt.savefig('PowerSpec.png')
  	return 
+
+def gen_spcgrm(tankname,channel,cutoffs=(0,250),binsize=50):
+	r = io.TdtIO(dirname=tankname)
+	bl = r.read_block(lazy=False,cascade=True)
+	for analogsig in bl.segments[0].analogsignals:
+		if analogsig.name[:4]=='LFP2':
+			analogsig.channel_index +=96
+		if (analogsig.name[:3]=='LFP')&(analogsig.channel_index==channel):
+			data = analogsig
+	srate = analogsig.sampling_rate
+    pxxx,freqs,bins,im=specgram(data,Fs=srate,NFFT=binsize,noverlap=0)
+        if i==0:
+            spec = pxxx.copy()
+        spec = pxxx + spec
+    
+    return spec/lfpdat.shape[1],freqs,bins
