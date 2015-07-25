208f2cab512836d5d3f22534ca490982df1b82dd
srsummerson <srsummerson@gmail.com>
2015-07-25 15:06:43 -0700

    fig code

--- spectralAnalysis.py
+++ spectralAnalysis.py
@@ -62,9 +62,9 @@ def LFPSpectrumAllChannel(tankname,num_channels):
  					
  				plt.plot(freq,Pxx_den/np.sum(Pxx_den),'r')
  				ax1.set_xlim([0, 100])
- 				#ax1.set_xticklabels([])
+ 				ax1.set_xticklabels([])
 				ax1.set_ylim([0, 1.0])
-				#ax1.set_yticklabels([])
+				ax1.set_yticklabels([])
 				plt.title(str(analogsig.channel_index))
  				
  				plt.figure(2)
@@ -75,8 +75,8 @@ def LFPSpectrumAllChannel(tankname,num_channels):
 				plt.semilogy(freq,Pxx_den/np.sum(Pxx_den),'r')
  				ax2.set_xlim([0, 100])
  				ax2.set_xticklabels([])
-				ax2.set_ylim([0, 1.0e-8])
-				ax2.set_yticklabels([])
+				#ax2.set_ylim([0, 1.0e-8])
+				#ax2.set_yticklabels([])
 				plt.title(str(analogsig.channel_index))
  		plt.figure(1)
  		plt.savefig('NormalizedPowerSpec_'+tank+'_'+str(block_num)+'.png')
