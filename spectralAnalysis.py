f87539821a6a8875da3b11a05323af7d6cc6b4a1
srsummerson <srsummerson@gmail.com>
2015-07-25 14:19:26 -0700

    log scale again

--- spectralAnalysis.py
+++ spectralAnalysis.py
@@ -20,24 +20,15 @@ def LFPSpectrumSingleChannel(tankname,channel):
 			if (analogsig.name[:3]=='LFP')&(analogsig.channel_index==channel):
 				Fs = analogsig.sampling_rate
 				data = analogsig
-				data_times = analogsig.times
-				n = len(data) # length of the signal
-				k = arange(n)
- 				T = n/Fs
- 				frq = k/T # two sides frequency range
- 				#frq = frq[range(n/2)] # one side frequency range
- 				
- 				frq = frq[:100000]
- 				#Welch method, see: http://docs.scipy.org/doc/scipy-dev/reference/generated/scipy.signal.welch.html
- 				#freq, Pxx_den = signal.welch(data, Fs, nperseg=1024)
- 				Y = fft(data)/n # fft computing and normalization
- 				#Y = Y[range(n/2)]
- 				Y = Y[:100000]
+				Fs = analogsig.sampling_rate
+				data = analogsig
+				
+ 				freq, Pxx_den = signal.welch(data, Fs, nperseg=1024)
  				plt.figure()
- 				plt.plot(frq,abs(Y),'r') # plotting the spectrum
+ 				plt.plot(freq,Pxx_den/np.sum(Pxx_den),'r') # plotting the spectrum
  				#plt.yscale('log')
  				plt.xlabel('Freq (Hz)')
- 				plt.ylabel('|Y(freq)|')
+ 				plt.ylabel('PSD')
  				#title('Channel %f' %channel)
  				plt.savefig('PowerSpec_Channel'+str(channel)+'.png')
 
@@ -51,7 +42,7 @@ def LFPSpectrumAllChannel(tankname,num_channels):
 	block_num = 0
 	r = io.TdtIO(dirname=tankname)
 	bl = r.read_block(lazy=False,cascade=True)
-	plt.figure()
+	
 	matplotlib.rcParams.update({'font.size': 6})
 	for block in bl.segments:
 		block_num += 1
@@ -63,21 +54,33 @@ def LFPSpectrumAllChannel(tankname,num_channels):
 				data = analogsig
 				
  				freq, Pxx_den = signal.welch(data, Fs, nperseg=1024)
- 				
+ 				plt.figure(1)
  				if num_channels==96:
  					ax1 = plt.subplot(8,12,analogsig.channel_index)
  				else:
  					ax1 = plt.subplot(10,16,analogsig.channel_index)
-
+ 					
  				plt.plot(freq,Pxx_den/np.sum(Pxx_den),'r')
  				ax1.set_xlim([0, 100])
  				ax1.set_xticklabels([])
 				#ax1.set_ylim([0, 1.0e-8])
 				ax1.set_yticklabels([])
 				plt.title(str(analogsig.channel_index))
- 				#plt.yscale('log')
- 				#plt.xlabel('Freq (Hz)')
- 				#plt.ylabel('|Y(freq)|')
+ 				
+ 				plt.figure(2)
+ 				if num_channels==96:
+ 					ax2 = plt.subplot(8,12,analogsig.channel_index)
+ 				else:
+ 					ax2 = plt.subplot(10,16,analogsig.channel_index)
+				plt.plot(freq,Pxx_den,'r')
+ 				ax2.set_xlim([0, 100])
+ 				ax2set_xticklabels([])
+				#ax1.set_ylim([0, 1.0e-8])
+				ax2.set_yticklabels([])
+				plt.title(str(analogsig.channel_index))
+ 		plt.figure(1)
+ 		plt.savefig('NormalizedPowerSpec_'+tank+'_'+str(block_num)+'.png')
+ 		plt.figure(2)
  		plt.savefig('PowerSpec_'+tank+'_'+str(block_num)+'.png')
  	return 
 
