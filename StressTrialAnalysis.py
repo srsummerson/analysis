2c2f81b4c13978018c6b9cef6ba1a8fb9f6d2328
srsummerson <srsummerson@gmail.com>
2016-01-27 18:08:31 -0800

    updating for HDEEG analysis and figs

--- StressTrialAnalysis.py
+++ StressTrialAnalysis.py
@@ -304,19 +304,39 @@ for i in range(0,len(row_ind_successful_stress)):
 	pupil_stress_std.append(np.mean(pupil_snippet))
 
 Fs = hdeeg_samprate
+density_length = 30
 for chann in hdeeg.keys:
-	trial_power = dict() 	# power computed per frequency over all time
-	beta_power = dict()		# power computed per time interval over beta band
+	trial_power = np.zeros([len(row_ind_successful_stress),density_length])
+	beta_power = np.zeros([len(row_ind_successful_stress),10])
 	for i in range(0,len(row_ind_successful_stress)):	
 		hdeeg_snippet = hdeeg[channel][hdeeg_ind_successful_stress[i]:hdeeg_ind_successful_stress[i]+samples_hdeeg_successful_stress[i]]
 		num_timedom_samples = hdeeg_snippet.size
 		time = [float(t)/Fs for t in range(0,num_timedom_samples)]
  		freq, Pxx_den = signal.welch(hdeeg_snippet, Fs, nperseg=1024)
- 		trial_power[num2str(i)] = Pxx_den
+ 		trial_power[i][:] = Pxx_den[0:density_length]
  		hdeeg_snippet_aligned_to_end = hdeeg_snippet[-hdeeg_samprate:]
  		num_timedom_samples = hdeeg_snippet_aligned_to_end.size
  		time = [float(t)/Fs for t in range(0,num_timedom_samples)]
  		Pxx, freqs, bins, im = plt.specgram(x, NFFT=512, Fs=Fs, noverlap=256)
+ 		freq_beta = np.logical_and(np.greater(freqs,10),np.less(freqs,30))
+ 		freq_beta_ind = np.ravel(np.nonzero(freq_beta))
+ 		Pxx_beta = np.sum(Pxx[freq_beta_ind],axis=0)
+ 		beta_power[i][:] = Pxx_beta
+ 	# plot figures here
+ 	z_min, z_max = -np.abs(trial_power).max(), np.abs(trial_power).max()
+	plt.figure()
+	plt.subplot(1, 2, 1)
+	plt.pcolor(freq[0:density_length],range(0,len(row_ind_successful_stress)),trial_power, cmap='RdBu', vmin=z_min, vmax=z_max)
+	plt.title('Trial Power')
+	plt.colorbar()
+	z_min, z_max = -np.abs(beta_power).max(), np.abs(beta_power).max()
+	plt.subplot(1, 2, 2)
+	plt.pcolor(bins,range(0,len(row_ind_successful_stress)),beta_power, cmap='RdBu', vmin=z_min, vmax=z_max)
+	plt.title('Spectrogram: 10 - 30 Hz power')
+	# set the limits of the plot to the limits of the data
+	#plt.axis([x.min(), x.max(), y.min(), y.max()])
+	plt.colorbar()
+	plt.show()
 '''
 Stopped here.
 '''
