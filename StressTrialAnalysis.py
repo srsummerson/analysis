a363b18c0be6756440fc7e073fc50706a9b22fbd
srsummerson <srsummerson@gmail.com>
2016-02-02 09:14:21 -0800

    changing day

--- StressTrialAnalysis.py
+++ StressTrialAnalysis.py
@@ -52,12 +52,12 @@ def running_mean(x, N):
 	return (cumsum[N:] - cumsum[:-N]) / float(N) 
 
 # Set up code for particular day and block
-hdf_filename = 'luig20151204_07.hdf'
-filename = 'Luigi20151204_HDEEG'
+hdf_filename = 'luig20151229_02.hdf'
+filename = 'Luigi20151229_HDEEG'
 TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 hdf_location = '/storage/rawdata/hdf/'+hdf_filename
 #hdf_location = hdf_filename
-block_num = 2
+block_num = 1
 
 num_avg = 50 	# number of trials to compute running average of trial statistics over
 
