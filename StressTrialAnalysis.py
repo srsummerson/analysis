a91ad54c3ec8528f0f4fc046aec6d2f2694c6532
srsummerson <srsummerson@gmail.com>
2016-04-12 19:55:14 -0700

    mario analysis 4/12

--- StressTrialAnalysis.py
+++ StressTrialAnalysis.py
@@ -15,12 +15,12 @@ from spectralAnalysis import TrialAveragedPSD
 
 
 # Set up code for particular day and block
-hdf_filename = 'mari20160409_02_te1956.hdf'
-filename = 'Mario20160409'
+hdf_filename = 'mari20160412_07_te1982.hdf'
+filename = 'Mario20160412'
 TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 hdf_location = '/storage/rawdata/hdf/'+hdf_filename
 #hdf_location = hdffilename
-block_num = 1
+block_num = 3
 stim_freq = 100
 
 lfp1_channels = [34, 39, 44, 45, 71, 76, 80, 82, 84, 90, 93, 94, 95, 96]
