6edc5d230a12e5932fb38fd519c04c9e970b298a
srsummerson <srsummerson@gmail.com>
2016-05-17 19:29:30 -0700

    stress plots for 5/17

--- StressTrialAnalysis.py
+++ StressTrialAnalysis.py
@@ -15,12 +15,12 @@ from spectralAnalysis import TrialAveragedPSD
 
 
 # Set up code for particular day and block
-hdf_filename = 'mari20160516_03_te2088.hdf'
-filename = 'Mario20160516'
+hdf_filename = 'mari20160517_09_te2099.hdf'
+filename = 'Mario20160517'
 TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 hdf_location = '/storage/rawdata/hdf/'+hdf_filename
 #hdf_location = hdffilename
-block_num = 1
+block_num = 2
 stim_freq = 100
 
 lfp1_channels = [13, 14, 15, 16, 29, 30]
