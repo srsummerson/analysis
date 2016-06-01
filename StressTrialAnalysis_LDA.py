b0c5c91b536044c8b5a9adbc2de71ff6a2aef0b6
srsummerson <srsummerson@gmail.com>
2016-06-01 16:51:17 -0700

    switching order of elements in data matrix

--- StressTrialAnalysis_LDA.py
+++ StressTrialAnalysis_LDA.py
@@ -234,8 +234,8 @@ X_reg = []
 
 for i, ind in enumerate(lfp_ind_successful_stress):
 	trial_array = []
-	trial_array.append(pupil_stress_mean[i])
 	trial_array.append(ibi_stress_mean[i])
+	trial_array.append(pupil_stress_mean[i])
 	
 		
 	for chann in lfp_channels:
@@ -247,8 +247,8 @@ for i, ind in enumerate(lfp_ind_successful_stress):
 
 for i, ind in enumerate(lfp_ind_stress):
 	trial_array = []
-	trial_array.append(pupil_all_stress_mean[i])
 	trial_array.append(ibi_all_stress_mean[i])
+	trial_array.append(pupil_all_stress_mean[i])
 		
 	for chann in lfp_channels:
 		freq, Pxx_den = signal.welch(lfp[chann][ind:ind+samples_lfp_stress[i]], lfp_samprate, nperseg=1024)
@@ -259,8 +259,8 @@ for i, ind in enumerate(lfp_ind_stress):
 
 for i, ind in enumerate(lfp_ind_successful_reg):
 	trial_array = []
-	trial_array.append(pupil_reg_mean[i])
 	trial_array.append(ibi_reg_mean[i])
+	trial_array.append(pupil_reg_mean[i])
 	
 		
 	for chann in lfp_channels:
@@ -272,8 +272,8 @@ for i, ind in enumerate(lfp_ind_successful_reg):
 
 for i, ind in enumerate(lfp_ind_reg):
 	trial_array = []
-	trial_array.append(pupil_all_reg_mean[i])
 	trial_array.append(ibi_all_reg_mean[i])
+	trial_array.append(pupil_all_reg_mean[i])
 		
 	for chann in lfp_channels:
 		freq, Pxx_den = signal.welch(lfp[chann][ind:ind+samples_lfp_reg[i]], lfp_samprate, nperseg=1024)
