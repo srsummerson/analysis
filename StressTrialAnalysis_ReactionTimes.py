ff8cc86f8bd6526417af8914f4a62ed63ed7b50a
srsummerson <srsummerson@gmail.com>
2016-07-06 12:38:46 -0700

    different day

--- StressTrialAnalysis_ReactionTimes.py
+++ StressTrialAnalysis_ReactionTimes.py
@@ -14,9 +14,9 @@ from probabilisticRewardTaskPerformance import FreeChoiceBehavior_withStressTria
 from spectralAnalysis import TrialAveragedPSD
 from rt_calc import compute_rt_per_trial_StressTask
 
-hdf_filename = 'mari20160319_15_te1793.hdf'
+hdf_filename = 'mari20160325_03_te1852.hdf'
 hdf_location = '/storage/rawdata/hdf/'+hdf_filename
-filename = 'Mario20160319'
+filename = 'Mario20160325'
 block_num = 1
 
 
