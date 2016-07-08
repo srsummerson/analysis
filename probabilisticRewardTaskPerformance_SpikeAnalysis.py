f550dc7c6b4452778fe645222a3188c24db17771
srsummerson <srsummerson@gmail.com>
2016-07-08 10:32:58 -0700

    changing how plx files are found

--- probabilisticRewardTaskPerformance_SpikeAnalysis.py
+++ probabilisticRewardTaskPerformance_SpikeAnalysis.py
@@ -297,13 +297,14 @@ def probabilisticRewardTask_PSTH_SepSpikeFiles(hdf_filename, filename, block_num
 	total_units = 0
 	
 	print "Getting spike data."
-	eNe1_channs = range(1,97)
-	eNe2_channs = range(1,65)
-	for chann in eNe1_channs:
-		plx_filename1 = plx_filename1_prefix + '_CH'+str(chann)+'.plx'
-		plx_location1 = '/backup/subnetsrig/storage/tdt/'+filename+'/'+'Block-'+ str(block_num) + '/'+plx_filename1
+	plx_location1 = TDT_tank + '/'+'Block-'+ str(block_num) + '/'
+	plx_location2 = TDT_tank + '/'+'Block-'+ str(block_num) + '/'
+	eNe1_channs = glob.glob(plx_location1+'Offline_eNe1_*.plx')
+	eNe2_channs = glob.glob(plx_location1+'Offline_eNe2_*.plx')
+	for plx_data in eNe1_channs:
+		chann = int(plx_data[len(plx_location1)+len('Offline_eNe1_CH'):-len('.plx')])
 		# Get spike data
-		plx1 = plexfile.openFile(plx_location1)
+		plx1 = plexfile.openFile(plx_data)
 		#spike_file = plx1.spikes[:].data
 		#psth_all_trials[str(chann)], smooth_psth_all_trials[str(chann)], labels_all_trials = computePSTH_SingleChannel(spike_file,plx_filename1,neural_data_center_hold_times,window_before,window_after, binsize)
 		#psth_lv_trials[str(chann)], smooth_psth_lv_trials[str(chann)], labels_lv_trials = computePSTH_SingleChannel(spike_file,plx_filename1,neural_data_center_hold_times[choose_lv],window_before,window_after, binsize)
@@ -311,11 +312,10 @@ def probabilisticRewardTask_PSTH_SepSpikeFiles(hdf_filename, filename, block_num
 
 		#total_units += len(labels_all_trials)
 		print total_units
-	for chann in eNe2_channs:
-		plx_filename2 = plx_filename2_prefix + '_CH'+str(chann)+'.plx'
-		plx_location2 = '/backup/subnetsrig/storage/tdt/'+filename+'/'+'Block-'+ str(block_num) + '/'+plx_filename2
+	for plx_data in eNe2_channs:
+		chann = int(plx_data[len(plx_location1)+len('Offline_eNe2_CH'):-len('.plx')])+96
 		# Get spike data
-		plx2 = plexfile.openFile(plx_location2)
+		plx2 = plexfile.openFile(plx_data)
 		#spike_file = plx2.spikes[:].data
 		#psth_all_trials[str(chann)+96], smooth_psth_all_trials[str(chann)+96], labels_all_trials = computePSTH_SingleChannel(spike_file,plx_filename2,neural_data_center_hold_times,window_before,window_after, binsize)
 		#psth_lv_trials[str(chann)+96], smooth_psth_lv_trials[str(chann)+96], labels_lv_trials = computePSTH_SingleChannel(spike_file,plx_filename2,neural_data_center_hold_times[choose_lv],window_before,window_after, binsize)
