054dabefbb3eb7032aa391ad52af32006cd0dde5
srsummerson <srsummerson@gmail.com>
2016-07-08 12:05:01 -0700

    not extracting spikes

--- probabilisticRewardTaskPerformance_SpikeAnalysis.py
+++ probabilisticRewardTaskPerformance_SpikeAnalysis.py
@@ -308,7 +308,7 @@ def probabilisticRewardTask_PSTH_SepSpikeFiles(hdf_filename, filename, block_num
 		all_channs.append(chann)
 		# Get spike data
 		plx1 = plexfile.openFile(plx_data)
-		spike_file = plx1.spikes[:].data
+		#spike_file = plx1.spikes[:].data
 		#psth, smooth_psth, labels_all_trials = computePSTH_SingleChannel(spike_file,chann,neural_data_center_hold_times,window_before,window_after, binsize)
 		#psth_all_trials[str(chann)] = psth
 		#smooth_psth_all_trials[str(chann)] = smooth_psth 
@@ -322,7 +322,7 @@ def probabilisticRewardTask_PSTH_SepSpikeFiles(hdf_filename, filename, block_num
 		all_channs.append(chann)
 		# Get spike data
 		plx2 = plexfile.openFile(plx_data)
-		spike_file = plx2.spikes[:].data
+		#spike_file = plx2.spikes[:].data
 		#psth_all_trials[str(chann)+96], smooth_psth_all_trials[str(chann)+96], labels_all_trials = computePSTH_SingleChannel(spike_file,chann,neural_data_center_hold_times,window_before,window_after, binsize)
 		#psth_lv_trials[str(chann)+96], smooth_psth_lv_trials[str(chann)+96], labels_lv_trials = computePSTH_SingleChannel(spike_file,chann,neural_data_center_hold_times[choose_lv],window_before,window_after, binsize)
 		#psth_hv_trials[str(chann)+96], smooth_psth_hv_trials[str(chann)+96], labels_hv_trials = computePSTH_SingleChannel(spike_file,chann,neural_data_center_hold_times[choose_hv],window_before,window_after, binsize)
