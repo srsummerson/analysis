2bf446128603876f3354d39203d7c5c4b1543cf4
srsummerson <srsummerson@gmail.com>
2016-12-21 19:38:47 -0800

    updated figs for three target task with stimulation

--- syncHDF.py
+++ syncHDF.py
@@ -4,7 +4,7 @@ from neo import io
 
 
 
-filename = 'Mario20161219'
+filename = 'Mario20161221'
 #TDT_tank = '/backup/subnetsrig/storage/tdt/'+filename
 TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 #block_num = 1
