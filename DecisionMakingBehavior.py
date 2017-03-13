3031cfdaa1e7e75046e661356527daf4b8b2d10b
srsummerson <srsummerson@gmail.com>
2017-03-13 11:18:08 -0700

    fixing indentation errors

--- DecisionMakingBehavior.py
+++ DecisionMakingBehavior.py
@@ -812,18 +812,17 @@ def ThreeTargetTask_RegressFiringRates_PictureOnset(hdf_files, syncHDF_files, sp
 
 	# 3. Get Q-values
 	if var_value:
-		# Varying Q-values
+	# Varying Q-values
 		targets_on, chosen_target, rewards, instructed_or_freechoice = cb.GetChoicesAndRewards
 		# Find ML fit of alpha and beta
 		Q_initial = 0.5*np.ones(3)
 		nll = lambda *args: -loglikelihood_ThreeTargetTask_Qlearning(*args)
 		result = op.minimize(nll, [alpha_true, beta_true], args=(Q_initial, chosen_target, rewards, targets_on, instructed_or_freechoice), bounds=[(0,1),(0,None)])
-    	alpha_ml, beta_ml = result["x"]
-    	# RL model fit for Q values
-   		Q_low, Q_mid, Q_high, prob_choice_low, prob_choice_mid, prob_choice_high = loglikelihood_ThreeTargetTask_Qlearning([alpha_ml, beta_ml], Q_initial, chosen_target, rewards, targets_on, instructed_or_freechoice)
-	
+		alpha_ml, beta_ml = result["x"]
+		# RL model fit for Q values
+		Q_low, Q_mid, Q_high, prob_choice_low, prob_choice_mid, prob_choice_high = loglikelihood_ThreeTargetTask_Qlearning([alpha_ml, beta_ml], Q_initial, chosen_target, rewards, targets_on, instructed_or_freechoice)
 	else:
-		# Fixed Q-values
+	# Fixed Q-values
 		Q_low = 0.35*np.ones(total_trials)
 		Q_mid = 0.6*np.ones(total_trials)
 		Q_high = 0.85*np.ones(total_trials)
