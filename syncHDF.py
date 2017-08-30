0c4970eb9d6c6255922354526ddf65039d92edcc
srsummerson <srsummerson@gmail.com>
2017-08-30 10:15:16 -0700

    changing days

--- syncHDF.py
+++ syncHDF.py
@@ -4,7 +4,7 @@ from neo import io
 
 
 
-filename = 'Luigi20170824'
+filename = 'Luigi20170822'
 #TDT_tank = '/backup/subnetsrig/storage/tdt/'+filename
 TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 #TDT_tank = './' + filename
