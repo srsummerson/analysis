cb4b76f56083d188c9c84bf72efd6e620c85a562
srsummerson <srsummerson@gmail.com>
2018-06-07 12:37:33 -0700

    updating day

--- StressTrialAnalysis_LDA_PowerFeatures.py
+++ StressTrialAnalysis_LDA_PowerFeatures.py
@@ -37,11 +37,11 @@ Need to check lfp_center_states
 """
 
 
-hdf_filename = 'mari20160402_04_te1923.hdf'
+hdf_filename = 'mari20160404_04_te1927.hdf'
 hdf_filename_stim = ''
-filename = 'Mario20160402'
-filename2 = 'Mario20160402'
-block_num = 2
+filename = 'Mario20160404'
+filename2 = 'Mario20160404'
+block_num = 1
 block_num_stim = 2
 print filename
 TDT_tank = '/backup/subnetsrig/storage/tdt/'+filename
