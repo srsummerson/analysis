ddcbbbd1c67a43a064c1996a2cc5b511847b87e0
srsummerson <srsummerson@gmail.com>
2018-09-20 18:48:10 -0700

    changing directory for data

--- syncHDF.py
+++ syncHDF.py
@@ -6,8 +6,8 @@ from neo import io
 
 
 filename = 'Mario20180831'
-#TDT_tank = '/backup/subnetsrig/storage/tdt/'+filename
-TDT_tank = '/home/srsummerson/storage/tdt/'+filename
+TDT_tank = '/backup/subnetsrig/storage/tdt/'+filename
+#TDT_tank = '/home/srsummerson/storage/tdt/'+filename
 #TDT_tank = './' + filename
 #block_num = 1
 
